0e65f67ec3ab0b12e93790a68fcc2c98a970c0ed
Hogu59 <dbstjdguswkd@naver.com>
2025-02-20 00:48:21 +0900

    feat(presentation): implement HomeAction and State

---
+++ app/src/main/java/com/ottf/quizdroid/presentation/home/HomeViewModel.kt
@@ -0,0 +1,43 @@
+package com.ottf.quizdroid.presentation.home
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+class HomeViewModel : ViewModel() {
+    private val _state = MutableStateFlow(HomeState())
+    val state = _state
+        .onStart {
+            fetchToday()
+        }.stateIn(
+            viewModelScope,
+            SharingStarted.WhileSubscribed(5000L),
+            _state.value,
+        )
+
+    fun onAction(action: HomeAction) {
+        when (action) {
+            is HomeAction.OnNavigateToQuiz -> {
+            }
+
+            is HomeAction.OnNavigateToStatistics -> {
+            }
+
+            is HomeAction.OnNavigateToSettings -> {
+            }
+        }
+    }
+
+    private fun fetchToday() {
+        val currentDate = LocalDate.now()
+        val formatter = DateTimeFormatter.ofPattern("yyyy년 MM월 dd일")
+        val formattedDate = currentDate.format(formatter)
+        _state.value = _state.value.copy(today = formattedDate)
+    }
+}

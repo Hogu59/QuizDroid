783dfbd56c75afc887a32452e96ff6402ad2f4f2
Hogu59 <dbstjdguswkd@naver.com>
2025-02-23 23:02:01 +0900

    feat(settings): implement components

---
+++ app/src/main/java/com/ottf/quizdroid/presentation/settings/components/SettingInfoItem.kt
@@ -0,0 +1,31 @@
+package com.ottf.quizdroid.presentation.settings.components
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun SettingInfoItem(
+    title: String,
+    value: String,
+    modifier: Modifier = Modifier,
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 12.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(title, fontSize = 14.sp)
+        Spacer(modifier = Modifier.weight(1f))
+        Text(value, fontSize = 14.sp, color = Color.Gray)
+    }
+}
---
+++ app/src/main/java/com/ottf/quizdroid/presentation/settings/components/SettingSwitchItem.kt
@@ -0,0 +1,65 @@
+package com.ottf.quizdroid.presentation.settings.components
+
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ottf.quizdroid.ui.theme.RobotoFontFamily
+
+@Composable
+fun SettingSwitchItem(
+    title: String,
+    checked: Boolean,
+    onCheckedChange: (Boolean) -> Unit,
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onCheckedChange(!checked) }
+            .padding(vertical = 12.dp),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        val animatedChecked by animateFloatAsState(
+            targetValue = if (checked) 1f else 0f,
+            animationSpec = tween(durationMillis = 500), // 애니메이션 속도 조절 (500ms)
+        )
+        Text(
+            text = title,
+            fontSize = 14.sp,
+            color = Color.Black,
+            fontFamily = RobotoFontFamily,
+            fontWeight = FontWeight.W300,
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        Switch(
+            checked = checked,
+            onCheckedChange = onCheckedChange,
+            modifier = Modifier
+                .width(56.dp)
+                .height(18.dp),
+            colors = SwitchDefaults.colors(
+                checkedThumbColor = Color.White,
+                checkedTrackColor = Color(0xFF000000),
+                uncheckedBorderColor = Color(0xFFBDBDBD),
+                uncheckedThumbColor = Color.White,
+                uncheckedTrackColor = Color(0xFFBDBDBD),
+            ),
+        )
+    }
+}

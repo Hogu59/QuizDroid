6efa648c7709eae14261e6c5054e09a480863904
Hogu59 <dbstjdguswkd@naver.com>
2025-02-23 23:02:01 +0900

    feat(settings): implement components

---
+++ app/src/main/java/com/ottf/quizdroid/presentation/settings/components/SettingCard.kt
@@ -0,0 +1,41 @@
+package com.ottf.quizdroid.presentation.settings.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.ottf.quizdroid.ui.theme.CustomTypography
+import com.ottf.quizdroid.ui.theme.Gray300
+
+@Composable
+fun SettingCard(
+    title: String,
+    content: @Composable () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(top = 16.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.White),
+        elevation = CardDefaults.cardElevation(0.5.dp),
+        shape = RoundedCornerShape(12.dp),
+    ) {
+        Column(modifier = Modifier.padding(horizontal = 16.dp)) {
+            Text(
+                text = title,
+                style = CustomTypography.labelMedium,
+                color = Gray300,
+                modifier = Modifier.padding(vertical = 12.dp),
+            )
+            content()
+        }
+    }
+}

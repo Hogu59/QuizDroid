da792dddd7496c097da05155f029e29d3c490f7d
Hogu59 <dbstjdguswkd@naver.com>
2025-02-27 23:10:25 +0900

    refactor: apply room db to presentation logic

--- app/src/main/java/com/ottfstudio/quizdroid/presentation/quiz/QuizViewModel.kt
+++ app/src/main/java/com/ottfstudio/quizdroid/presentation/quiz/QuizViewModel.kt
@@ -3,7 +3,9 @@ package com.ottfstudio.quizdroid.presentation.quiz
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.ottfstudio.quizdroid.domain.model.Record
 import com.ottfstudio.quizdroid.domain.repository.QuizRepository
+import com.ottfstudio.quizdroid.domain.repository.RecordRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
@@ -18,6 +20,7 @@ class QuizViewModel
     @Inject
     constructor(
         private val quizRepository: QuizRepository,
+        private val recordRepository: RecordRepository,
     ) : ViewModel() {
         private var _state = MutableStateFlow(QuizState())
         val state = _state.stateIn(
@@ -33,6 +36,8 @@ class QuizViewModel
         fun onAction(action: QuizAction) {
             when (action) {
                 is QuizAction.OnSubmitAnswer -> {
+                    _state.value = _state.value.copy(isSolved = true)
+                    solveQuiz()
                     _state.value = _state.value.copy(isShowAnswer = true)
                 }
 
@@ -50,17 +55,33 @@ class QuizViewModel
                 try {
                     _state.value = _state.value.copy(isLoading = true)
                     val today = fetchToday()
-                    val fetchedQuiz = quizRepository.fetchTodayQuiz(today)
-                    _state.value = _state.value.copy(
-                        quiz = fetchedQuiz,
-                        isLoading = false,
-                        selectedOption = _state.value.selectedOption,
-                    )
+                    val record = checkTodayRecord(today)
+
+                    if (record == null) {
+                        val fetchedQuiz = quizRepository.fetchTodayQuiz(today)
+                        _state.value = _state.value.copy(
+                            quiz = fetchedQuiz,
+                            isLoading = false,
+                            selectedOption = _state.value.selectedOption,
+                        )
+                    } else {
+                        _state.value = _state.value.copy(
+                            quiz = record.quiz,
+                            isLoading = false,
+                            selectedOption = record.selectedOption,
+                            isSolved = true,
+                            isShowAnswer = true,
+                        )
+                    }
                 } catch (e: Exception) {
                     _state.value = _state.value.copy(isLoading = false)
                     println("Failed to fetch quiz: $e")
                 }
             }
+        }
+
+        private suspend fun checkTodayRecord(date: String): Record? {
+            return recordRepository.fetchQuizRecord(date)
         }
 
         private fun fetchToday(): String {
@@ -69,10 +90,29 @@ class QuizViewModel
                 val formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd")
                 val formattedDate = currentDate.format(formatter)
                 require(formattedDate.isNotBlank()) { "Formatted date is blank" }
+                _state.value = _state.value.copy(today = formattedDate)
                 formattedDate
             } catch (e: Exception) {
                 Log.e("QuizViewModel", "Error fetching today", e)
                 throw IllegalStateException("Error fetching today", e)
+            }
+        }
+
+        private fun solveQuiz() {
+            viewModelScope.launch {
+                try {
+                    recordRepository.insertQuizRecord(
+                        Record(
+                            date = state.value.today,
+                            quiz = state.value.quiz,
+                            selectedOption = state.value.selectedOption ?: -1,
+                            isCorrect = state.value.quiz.answer == state.value.selectedOption,
+                            isSolved = state.value.isSolved,
+                        ),
+                    )
+                } catch (e: Exception) {
+                    println("Failed to solve quiz: $e")
+                }
             }
         }
     }

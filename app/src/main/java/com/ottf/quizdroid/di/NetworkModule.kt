490948427e4d59d21715579e184fee9b31d28197
Hogu59 <dbstjdguswkd@naver.com>
2025-02-27 12:57:00 +0900

    feat: implement api level

---
+++ app/src/main/java/com/ottf/quizdroid/di/NetworkModule.kt
@@ -0,0 +1,47 @@
+package com.ottf.quizdroid.di
+
+import com.ottf.quizdroid.BuildConfig
+import com.ottf.quizdroid.data.api.QuizService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object NetworkModule {
+    @Provides
+    @Singleton
+    fun provideRetrofitClient(): Retrofit {
+//        val interceptor =
+//            HttpLoggingInterceptor().apply {
+//                level = HttpLoggingInterceptor.Level.BODY
+//            }
+
+        val client =
+            OkHttpClient.Builder()
+                .apply {
+//                    addInterceptor(interceptor)
+                    connectTimeout(30, TimeUnit.SECONDS)
+                    readTimeout(20, TimeUnit.SECONDS)
+                    writeTimeout(25, TimeUnit.SECONDS)
+                }.build()
+
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideQuizService(retrofit: Retrofit): QuizService = retrofit.create(
+        QuizService::class.java,
+    )
+}

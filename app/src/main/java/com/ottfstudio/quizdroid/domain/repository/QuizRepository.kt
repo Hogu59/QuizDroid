19f567a178dc24cc9535ed94515202b5019489ec
Hogu59 <dbstjdguswkd@naver.com>
2025-03-19 15:37:42 +0900

    fix: modify kDoc which is written wrongly
     - before) date format ("yyyy-MM-dd")
     - after) date formate ("yyyy.MM.dd")

--- app/src/main/java/com/ottfstudio/quizdroid/domain/repository/QuizRepository.kt
+++ app/src/main/java/com/ottfstudio/quizdroid/domain/repository/QuizRepository.kt
@@ -6,7 +6,7 @@ interface QuizRepository {
     /**
      * 지정된 날짜에 해당하는 퀴즈를 가져옵니다.
      *
-     * @param date 조회할 날짜 (형식: "yyyy-MM-dd")
+     * @param date 조회할 날짜 (형식: "yyyy.MM.dd")
      * @return 해당 날짜의 퀴즈
      */
     suspend fun fetchTodayQuiz(date: String): Quiz

ef9d71253af78ad65668d358b9ffb4e408b99820
Hogu59 <dbstjdguswkd@naver.com>
2025-03-19 15:52:01 +0900

    chore: modify kDocs

--- app/src/main/java/com/ottfstudio/quizdroid/data/local/RecordDao.kt
+++ app/src/main/java/com/ottfstudio/quizdroid/data/local/RecordDao.kt
@@ -26,6 +26,12 @@ interface RecordDao {
     @Query("SELECT * FROM quiz_record WHERE date = :date")
     suspend fun fetchQuizRecordByDate(date: String): QuizRecord?
 
+    /*** 지정된 날짜 범위 내에 해결된 퀴즈의 수를 조회합니다.
+     *
+     * @param startDate 조회 시작 날짜 (형식: "yyyy.MM.dd")
+     * @param endDate 조회 종료 날짜 (형식: "yyyy.MM.dd")
+     * @return 지정된 날짜 범위 내에 해결된 퀴즈의 수
+     */
     @Query("SELECT COUNT(*) FROM quiz_record WHERE date BETWEEN :startDate AND :endDate")
     suspend fun fetchSolvedCountByDateRange(startDate: String, endDate: String): Int
 
